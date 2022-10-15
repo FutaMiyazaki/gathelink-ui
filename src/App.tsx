6dc387b5eb82de91a7c36f5ab23352d5babda538
capcap-jp <miyamiyacap@gmail.com>
2022-10-15 19:44:12 +0900

    🔧 絶対パスでインポートできるように設定変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
+import Button from '@mui/material/Button'
+import CssBaseline from '@mui/material/CssBaseline'
 import { FC } from 'react'
 
-import logo from './logo.svg'
-import './App.css'
+import logo from '@/logo.svg'
+import '@/App.css'
 
 const App: FC = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+        </header>
+        <Button variant='contained'>こんにちは</Button>
+      </div>
+    </>
   )
 }
 
