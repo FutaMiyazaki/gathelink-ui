5e3f1b1341d8099eca062b1a42b2a03cc23c502d
capcap-jp <miyamiyacap@gmail.com>
2022-10-18 02:01:53 +0900

    Add draft routes components

---
+++ src/features/folder/routes/about.tsx
@@ -0,0 +1,4 @@
+import { Typography } from '@mui/material'
+import { FC } from 'react'
+
+export const FolderAbout: FC = () => <Typography>フォルダの詳細ページです</Typography>
---
+++ src/features/folder/routes/edit.tsx
@@ -0,0 +1,4 @@
+import { Typography } from '@mui/material'
+import { FC } from 'react'
+
+export const FolderEdit: FC = () => <Typography>フォルダ編集ページです</Typography>
