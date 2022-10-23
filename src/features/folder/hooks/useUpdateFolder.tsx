a84b901e4b4400ec99d34c66ac8767267de2e77d
capcap-jp <miyamiyacap@gmail.com>
2022-10-23 14:40:08 +0900

    ✨ フォルダの編集機能を追加

---
+++ src/features/folder/hooks/useUpdateFolder.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSetRecoilState } from 'recoil'
+
+import { apiClient } from '@/lib/axios/apiClient'
+import { alertState } from '@/states/AlertAtom'
+import { authHeaders } from '@/utils/authHeaders'
+
+type UseUpdateFolder = {
+  updateFolder: (folder: params, folderId: string) => Promise<void>
+  errorMessage: string
+  isLoading: boolean
+}
+
+type params = {
+  name: string
+}
+
+export const useUpdateFolder = (): UseUpdateFolder => {
+  const [errorMessage, setErrorMessage] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const setAlert = useSetRecoilState(alertState)
+  const headers = authHeaders()
+  const navigate = useNavigate()
+
+  const updateFolder = async (folder: params, folderId: string): Promise<void> => {
+    setIsLoading(true)
+    setErrorMessage('')
+
+    await apiClient
+      .patch(`/folders/${folderId}`, { folder }, { headers })
+      .then((res) => {
+        setAlert({
+          isShow: true,
+          message: `${res.data.name as string} フォルダを編集しました`,
+          severity: 'success',
+        })
+        navigate(`/folder/${folderId}`)
+      })
+      .catch((err) => {
+        setErrorMessage(err.message)
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }
+
+  return {
+    updateFolder,
+    errorMessage,
+    isLoading,
+  }
+}
--- src/features/folder/routes/edit.tsx
+++ src/features/folder/routes/edit.tsx
@@ -1,4 +1,107 @@
-import { Typography } from '@mui/material'
-import { FC } from 'react'
+import Alert from '@mui/material/Alert'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+import Stack from '@mui/material/Stack'
+import Switch from '@mui/material/Switch'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
+import { FC, useEffect } from 'react'
+import { Controller, SubmitHandler, useForm } from 'react-hook-form'
+import { useParams } from 'react-router-dom'
 
-export const FolderEdit: FC = () => <Typography>フォルダ編集ページです</Typography>
+import { Button } from '@/components/Elements/Button'
+import { LinkButton } from '@/components/Elements/Button/LinkButton'
+import { InputLabel } from '@/components/Elements/Form/InputLabel'
+import { PageLoading } from '@/components/Layouts/PageLoading'
+import { useFetchFolder } from '@/features/folder/hooks/useFetchFolder'
+import { useUpdateFolder } from '@/features/folder/hooks/useUpdateFolder'
+import { folderValidationRules } from '@/features/folder/utils/folderValidationRules'
+
+type Inputs = {
+  name: string
+}
+
+type RouterParams = {
+  folderId: string
+}
+
+export const FolderEdit: FC = () => {
+  const { control, handleSubmit } = useForm<Inputs>()
+  const { folderId } = useParams<RouterParams>()
+
+  const { fetchFolder, folder, isFeatchLoading } = useFetchFolder()
+  const { updateFolder, errorMessage, isLoading } = useUpdateFolder()
+
+  const onSubmit: SubmitHandler<Inputs> = (data: Inputs) => {
+    const folder = {
+      name: data.name,
+    }
+    updateFolder(folder, folderId as string)
+  }
+
+  useEffect(() => {
+    folderId !== undefined && fetchFolder(folderId)
+  }, [folderId])
+
+  if (isFeatchLoading) {
+    return <PageLoading />
+  }
+
+  return (
+    <Container maxWidth='sm' sx={{ mt: 4 }}>
+      <Typography component='h1' variant='h6' sx={{ fontWeight: 'bold', mb: 2 }}>
+        フォルダの編集
+      </Typography>
+      {errorMessage?.length !== 0 && (
+        <Alert severity='error' sx={{ mb: 2 }}>
+          {errorMessage}
+        </Alert>
+      )}
+      <Box sx={{ bgcolor: '#ffffff', borderRadius: 4, p: 2, mb: 3 }}>
+        <Box component='form' noValidate onSubmit={handleSubmit(onSubmit)}>
+          <InputLabel labelTitle='フォルダ名' />
+          <Controller
+            name='name'
+            control={control}
+            defaultValue={folder?.name}
+            rules={folderValidationRules.name}
+            render={({ field, fieldState }) => (
+              <TextField
+                {...field}
+                type='text'
+                fullWidth
+                autoFocus
+                size='small'
+                variant='standard'
+                error={fieldState.invalid}
+                helperText={fieldState.error?.message}
+                sx={{ mb: 4, px: 2 }}
+              />
+            )}
+          />
+          <Stack direction='row' justifyContent='space-between' sx={{ mb: 2 }}>
+            <InputLabel labelTitle='非公開にする' />
+            <Switch checked={true} />
+          </Stack>
+          <InputLabel labelTitle='説明' />
+          <TextField fullWidth multiline size='small' variant='standard' sx={{ mb: 4, px: 2 }} />
+          <Grid container alignItems='center' justifyContent='center' spacing={1}>
+            <Grid item xs={6}>
+              <LinkButton
+                color='secondary'
+                fullWidth={true}
+                label='キャンセル'
+                path={`/folder/${folderId as string}`}
+                variant='contained'
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <Button isLoading={isLoading} fullWidth={true} label='保存する' type='submit' />
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  )
+}
