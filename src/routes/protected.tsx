9dde9fd4b9cebfc8d4d978b44ffd7d04fa738c49
capcap-jp <miyamiyacap@gmail.com>
2022-10-23 19:35:01 +0900

    🔧 フォルダ更新、リンク更新コンポーネントへのルーティングを追加

--- src/routes/protected.tsx
+++ src/routes/protected.tsx
@@ -1,8 +1,28 @@
+import { FC } from 'react'
+import { Outlet } from 'react-router-dom'
+
+import { MainLayout } from '@/components/Layouts/MainLayout'
 import { FolderEdit } from '@/features/folder/routes/edit'
+import { EditLink } from '@/features/link/routes/EditLink'
+
+const Main: FC = () => (
+  <MainLayout>
+    <Outlet />
+  </MainLayout>
+)
 
 export const protectedRoutes = [
   {
-    path: '/folder/edit',
-    element: <FolderEdit />,
+    element: <Main />,
+    children: [
+      {
+        path: '/folder/:folderId/edit',
+        element: <FolderEdit />,
+      },
+      {
+        path: '/folder/:folderId/link/:linkId',
+        element: <EditLink />,
+      },
+    ],
   },
 ]
