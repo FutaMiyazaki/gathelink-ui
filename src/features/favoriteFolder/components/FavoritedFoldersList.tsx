4b95bb9afe76ac7acaa873b877b8bf685e63c444
capcap-jp <miyamiyacap@gmail.com>
2022-10-30 16:57:20 +0900

    ♻️ ソートボタンを削除、余白を修正

--- src/features/favoriteFolder/components/FavoritedFoldersList.tsx
+++ src/features/favoriteFolder/components/FavoritedFoldersList.tsx
@@ -1,10 +1,8 @@
 import FolderOpenRoundedIcon from '@mui/icons-material/FolderOpenRounded'
 import FolderRoundedIcon from '@mui/icons-material/FolderRounded'
-import SortIcon from '@mui/icons-material/Sort'
 import Alert from '@mui/material/Alert'
 import Box from '@mui/material/Box'
 import CircularProgress from '@mui/material/CircularProgress'
-import IconButton from '@mui/material/IconButton'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton'
@@ -12,16 +10,13 @@ import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
-import { FC, MouseEvent, useEffect, useState } from 'react'
+import { FC, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useRecoilValue } from 'recoil'
 
 import { Link } from '@/components/Elements/Link'
-import { Menu } from '@/components/Elements/Menu'
-import { MenuItems } from '@/components/Elements/Menu/MenuItems'
 import { useFetchFavoritedFolders } from '@/features/folder/hooks/useFetchFavoritedFolders'
 import { Folder } from '@/features/folder/types/Folder'
-import { FoldersSortType } from '@/features/folder/types/FoldersSortType'
 import { Link as LinkType } from '@/features/link/types/Link'
 import { favoritedFoldersState } from '@/states/FavoritedFoldersAtom'
 
@@ -87,43 +82,16 @@ export const FavoritedFoldersList: FC = () => {
   const { errorMessage, fetchFetchFavoritedFolders, isFeatching } = useFetchFavoritedFolders()
   const { folderId } = useParams<RouterParams>()
 
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
-
-  const handleChangeSort = (sortType: FoldersSortType): void => {
-    fetchFetchFavoritedFolders(sortType)
-    setAnchorEl(null)
-  }
-
-  const handleCloseMenu = (): void => setAnchorEl(null)
-
-  const handleOpenSortMenu = (e: MouseEvent<HTMLButtonElement>): void => {
-    setAnchorEl(e.currentTarget)
-  }
-
-  const sortMenuItems: MenuItems = [
-    { onClick: () => handleChangeSort('old'), text: '作成日時が古い順' },
-    { onClick: () => handleChangeSort('latest'), text: '作成日時が新しい順' },
-  ]
-
   useEffect(() => {
     fetchFetchFavoritedFolders('old')
   }, [])
 
   return (
-    <Box>
-      <Stack
-        alignItems='center'
-        direction='row'
-        justifyContent='space-between'
-        sx={{ pl: 2, pr: 1 }}
-      >
+    <Box sx={{ mb: 2 }}>
+      <Stack alignItems='center' direction='row' sx={{ pl: 2 }}>
         <Typography variant='subtitle1' color='secondary.dark' sx={{ fontWeight: 'bold' }}>
           お気に入り
         </Typography>
-        <IconButton onClick={handleOpenSortMenu}>
-          <SortIcon />
-        </IconButton>
-        <Menu anchorEl={anchorEl} handleCloseMenu={handleCloseMenu} menuItems={sortMenuItems} />
       </Stack>
       {isFeatching ? (
         <Stack justifyContent='center'>
