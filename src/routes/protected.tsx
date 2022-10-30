7e89eb631653e02c188327fecbcc1cfe9f19a8cf
capcap-jp <miyamiyacap@gmail.com>
2022-10-30 18:28:48 +0900

    🚚 Change filename

--- src/routes/protected.tsx
+++ src/routes/protected.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 import { MainLayout } from '@/components/Layouts/MainLayout'
 import { FavoritedFolders } from '@/features/favoriteFolder/routes/FavoritedFolders'
 import { EditFolder } from '@/features/folder/routes/EditFolder'
-import { MyFolders } from '@/features/folder/routes/MyFoldersList'
+import { MyFolders } from '@/features/folder/routes/MyFolders'
 import { EditLink } from '@/features/link/routes/EditLink'
 
 const Main: FC = () => (
