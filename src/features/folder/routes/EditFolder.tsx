c99484657c4b702362c1cfa6c7a4f04014e0910b
capcap-jp <miyamiyacap@gmail.com>
2022-11-12 14:12:07 +0900

    ♻️ フォルダ削除ボタンを配置

--- src/features/folder/routes/EditFolder.tsx
+++ src/features/folder/routes/EditFolder.tsx
@@ -1,10 +1,11 @@
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined'
 import Alert from '@mui/material/Alert'
 import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import IconButton from '@mui/material/IconButton'
 import TextField from '@mui/material/TextField'
-import { FC, useEffect } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { Controller, SubmitHandler, useForm } from 'react-hook-form'
 import { useParams } from 'react-router-dom'
 
@@ -13,6 +14,7 @@ import { InputLabel } from '@/components/Elements/Form/InputLabel'
 import { PageHeading } from '@/components/Elements/Heading/PageHeading'
 import { Link } from '@/components/Elements/Link'
 import { PageLoading } from '@/components/Layouts/PageLoading'
+import { DeleteFolderDialog } from '@/features/folder/components/Dialog/DeleteFolderDialog'
 import { useFetchFolder } from '@/features/folder/hooks/useFetchFolder'
 import { useUpdateFolder } from '@/features/folder/hooks/useUpdateFolder'
 import { folderValidationRules } from '@/features/folder/utils/folderValidationRules'
@@ -27,6 +29,7 @@ type RouterParams = {
 }
 
 export const EditFolder: FC = () => {
+  const [openConfirmDialog, setOpenConfirmDialog] = useState(false)
   const { control, handleSubmit } = useForm<Inputs>()
   const { folderId } = useParams<RouterParams>()
   const { fetchFolder, folder, isFeatchLoading } = useFetchFolder()
@@ -47,14 +50,22 @@ export const EditFolder: FC = () => {
   if (isFeatchLoading) return <PageLoading />
 
   return (
-    <Container maxWidth='sm'>
-      <Box sx={{ alignItems: 'center', display: 'flex', mb: 3 }}>
+    <Container maxWidth='md'>
+      <Box sx={{ display: 'flex', alignItems: 'center', mb: 3 }}>
         <Link path={`/folder/${folderId as string}`}>
           <IconButton component='span'>
             <ArrowBackIcon />
           </IconButton>
         </Link>
         <PageHeading text='フォルダを編集' sx={{ ml: 1 }} />
+        <Button
+          color='warning'
+          icon={<DeleteForeverOutlinedIcon />}
+          label='削除'
+          onClick={() => setOpenConfirmDialog(true)}
+          variant='text'
+          sx={{ ml: 'auto' }}
+        />
       </Box>
       {errorMessage !== '' && (
         <Alert icon={false} severity='error' sx={{ mb: 2 }}>
@@ -104,6 +115,11 @@ export const EditFolder: FC = () => {
           <Button isLoading={isLoading} label='保存する' type='submit' sx={{ mr: 2 }} />
         </Box>
       </Box>
+      <DeleteFolderDialog
+        folderId={folderId as string}
+        handleCloseDialog={() => setOpenConfirmDialog(false)}
+        open={openConfirmDialog}
+      />
     </Container>
   )
 }
